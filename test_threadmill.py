4d9517f9fbccc58e6d4fcb5621be89fbfbab61b8
Bhumika Singh Rathore <bhumikasinghrathore@gmail.com>
2017-06-13 16:10:56 +0530

    Noise margin is too high.

--- test_threadmill.py
+++ test_threadmill.py
@@ -26,9 +26,14 @@ import arduino
 
 res_file_ = '__results__tmp__.csv' 
 
+direc_ = 0
+dir_ = [ ]
 
 def stamp( ):
     return datetime.datetime.now().isoformat( )
+
+def smooth( vec, N ):
+    return np.convolve( vec, np.ones( N )/N, 'same' )
 
 def line_to_data(line):
     """First element is timestamp, rest the data etc.
@@ -77,6 +82,8 @@ def compute_speed( tvec, yvec ):
 
 def calculate_motion( t, s1, s2 ):
     """Caculate speed and direction of motion """
+    global dir_
+    global direc_
     t, s1, s2 = [ np.array( x ) for x in [ t, s1, s2] ]
     s1H = sig.hilbert( s1 )
     s2H = sig.hilbert( s2 )
@@ -84,21 +91,47 @@ def calculate_motion( t, s1, s2 ):
     v2 = compute_speed( t, s2 )
     # gpl.plot( (t, s1), (t, s2), terminal = 'X11' )
     speed = (v1+v2)/2.0
-    dire = calculate_direction( t[-1] - t[0], s1, s2 )
+    d = calculate_direction( t, s1, s2 )
+    if speed > 0:
+        dir_.append( d )
+        if len( dir_ ) > 100:
+            dir_.pop( 0 )
+        mean, std = np.mean( dir_ ), np.std( dir_ )
+        if d > mean + std:
+            direc_ = 1
+        elif d < mean - std:
+            direc_ = -1
+    else:
+        direc_ = 0
 
     with open( res_file_, 'a' ) as f:
-        f.write( '%g %g %g %g %g %g\n' % (t[-1], s1[-1], s2[-1], v1, v2, dire ) )
+        f.write( '%g %g %g %g %g %g\n' % (t[-1], s1[-1], s2[-1], v1, v2, direc_ ) )
 
-    return speed, dire
+    return speed, direc_
 
-def calculate_direction( dt, s1, s2 ):
+def calculate_direction( t, s1, s2 ):
     """Infer direction. 
     By computing the correlation.
     """
-    l = len( s1 )
-    x = sig.correlate( s1, s2, 'same' )
-    p = np.argmax( x ) - l / 2
-    return np.sign( p )
+    s1 = np.diff( s1 )
+    s2 = np.diff( s2 )
+    s1[ s1 < 0 ] = 0
+    s2[ s2 < 0 ] = 0
+    tvec, result = [ ], [ ]
+    startN = 0
+    startDetect = False
+    for i, x in enumerate( s1 ):
+        y = s2[ i ]
+        if x == 1:
+            if startN > 0:
+                tvec.append( t[i] - t[startN] )
+            startN = i
+            startDetect = True
+        if startDetect:
+            if y == 1:
+                startDetect = False
+                result.append( i - startN )
+    return 1000 * np.mean( result ) / np.mean( tvec )
 
 def main( port, baud ):
     print( '[INFO] Using port %s baudrate %d' % (port, baud ) )
@@ -119,13 +152,14 @@ def main( port, baud ):
         motion1.append( data[5] )
         motion2.append( data[6] )
         try:
-            N = 20
+            N = 50
             speed,direction = calculate_motion( tvec[-N:], motion1[-N:], motion2[-N:] )
         except Exception as e:
             speed = 0.0
             direction = 0
             print( e )
-        print( '%.5f %s' % (speed, direction))
+        if speed > 0:
+            print( '%.5f %s' % (speed, direction))
 
 if __name__ == '__main__':
     port = sys.argv[1]
